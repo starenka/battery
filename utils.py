a03a3764b43e7c337f07f839e748c7971bfee865
starenka <starenka0@gmail.com>
2012-11-23 20:14:47 +0100

    more cleanup

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ from pygame.mixer import Sound
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 SAMPLES_DIR = os.path.join(BASE_DIR, 'samples')
+BANKS_DIR = os.path.join(BASE_DIR, 'banks')
 
 def reverse_wav_file(file_path):
     """
@@ -22,11 +23,11 @@ def reverse_wav_file(file_path):
         f = wave.open(file_path)
         frames = f.getnframes()
         data = f.readframes(frames)
-        reversed = audioop.reverse(data, 2) # '2' is the sample width
+        rev = audioop.reverse(data, 2) # '2' is the sample width
         fr = wave.open(fr)
         # Must set params but leave # of frames empty
         fr.setparams((2, 2, 44100, '', 'NONE', 'not compressed'))
-        fr.writeframesraw(reversed)
+        fr.writeframesraw(rev)
     return reversed_file
 
 
@@ -53,32 +54,32 @@ def discover_samples(dir=SAMPLES_DIR):
     return itertools.cycle(samples)
 
 
-def load_banks(bank_kit):
+def load_banks(bank_kit, bank_dir=BANKS_DIR):
     """
         Loads bank kit definition from file and populates banks
         with Sound instances (both original wave & reversed one)
     """
-    global BASE_DIR
-    fname = os.path.join(BASE_DIR, 'banks', '%s.json' % bank_kit)
+    fname = os.path.join(bank_dir, '%s.json' % bank_kit)
 
     try:
         banks = json.load(open(fname))
-        iter_data = []
-        for i, bank in enumerate(banks, 1):
-            bank_data = {}
-            for key, slot in bank.iteritems():
-                if slot['sample']:
-                    samples = make_sample_tuples(slot)
-                    if samples:
-                        bank_data[key] = samples
-            iter_data.append((bank, bank_data, i))
-
-        return itertools.cycle(iter_data)
     except IOError:
         return None
+
+    iter_data = []
+    for i, bank in enumerate(banks, 1):
+        bank_data = {}
+        for key, slot in bank.iteritems():
+            if slot['sample']:
+                samples = make_sample_tuples(slot)
+                if samples:
+                    bank_data[key] = samples
+        iter_data.append((bank, bank_data, i))
+
+    return itertools.cycle(iter_data)
 
 def init_mixer(channels):
     # We need to init mixer before pygame initializations, smaller buffer should avoid lags
     pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=512)
     pygame.init()
-    pygame.mixer.set_num_channels(channels) # Get 8 channels for each key
+    pygame.mixer.set_num_channels(channels)
